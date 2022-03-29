8369b81627b2be39a8c7cb02012a5542a774067f
gunayakay <101050062+gunayakay@users.noreply.github.com>
2022-03-29 12:51:49 +0300

    Satt ödevi tamamlandı.

--- clock.js
+++ clock.js
@@ -1,1 +1,25 @@
-let guestName=prompt("Adınızı giriniz: ")
+let isimalani=document.querySelector("#myName");
+var isim= prompt("Adınızı Girin");
+if(isim.length>0){
+isimalani.innerHTML=isim;
+}
+else{
+isimalani.innerHTML="Misafir";
+}
+
+
+
+function showTime(){
+
+var tarih= new Date();
+var gun=tarih.getDay();
+  var saat=tarih.getHours();
+  var dakika=tarih.getMinutes();
+  var saniye=tarih.getSeconds();
+let gunler = ["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"];	
+document.getElementById("myClock").innerHTML=saat+":"+dakika+":"+saniye+" "+ gunler[gun];
+setTimeout(showTime, 1000); 
+
+}
+
+showTime();
